27cac7ad15a848606d66aa58d51b985d2275d79f
eduardo7321 <eduardo.jose184@gmail.com>
2024-05-07 23:28:38 -0300

    Exception for objects not found

--- src/main/java/com/edusilvasilva/helpdesk/services/TecnicoService.java
+++ src/main/java/com/edusilvasilva/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.edusilvasilva.helpdesk.domain.Tecnico;
 import com.edusilvasilva.helpdesk.repositories.TecnicoRepository;
+import com.edusilvasilva.helpdesk.services.exception.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado id: " + id));
 	}
 
 }
