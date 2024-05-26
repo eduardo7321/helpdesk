63e50844c3df12174eb3f755377f9bc86ba55963
eduardo7321 <eduardo.jose184@gmail.com>
2024-05-25 21:46:29 -0300

    DELETE method for technician implemented

--- src/main/java/com/edusilvasilva/helpdesk/resources/TecnicoResources.java
+++ src/main/java/com/edusilvasilva/helpdesk/resources/TecnicoResources.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,7 @@ public class TecnicoResources {
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
-	@GetMapping
+	@GetMapping(value ="/all")
 	public ResponseEntity<List<TecnicoDTO>> findAll() {
 		List<Tecnico> list = service.findAll();
 		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
@@ -54,5 +55,10 @@ public class TecnicoResources {
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
+	@DeleteMapping(value= "/{id}")
+	public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
